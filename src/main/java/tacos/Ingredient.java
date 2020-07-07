a31219cae4f5d228f2946a17f925ff5eabefe7b6
Bruce Berkowicz <bruceberk@me.com>
2020-07-07 12:24:50 -0400

    Refactored package name to match book source, implemented taco design page, model and controller (GET only)

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,21 @@
+package tacos;
+
+/**
+ * The Lombok library provides automatic method generation at runtime
+ */
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
